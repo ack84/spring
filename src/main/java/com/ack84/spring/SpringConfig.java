5ee0ffedbf1ca43faa7f5f3bc69976e178719522
ack84 <skwkfgo@naver.com>
2022-11-20 14:41:11 +0900

    spring jdbc

--- src/main/java/com/ack84/spring/SpringConfig.java
+++ src/main/java/com/ack84/spring/SpringConfig.java
@@ -1,14 +1,25 @@
 package com.ack84.spring;
 
 
+import com.ack84.spring.repository.JdbcTemplateRepository;
 import com.ack84.spring.repository.MemberRepository;
 import com.ack84.spring.repository.MemoryMemberRepository;
 import com.ack84.spring.service.MemberService;
+import hello.hellospring.repository.JdbcMemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     //생성자 주입
     @Bean
     public MemberService memberService(){
@@ -18,6 +29,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//      return new MemoryMemberRepository();
+//      return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateRepository(dataSource);
     }
 }
